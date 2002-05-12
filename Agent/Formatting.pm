c1ccee51101b1d8f08437547090991f0001dedf4
wendigo <wendigo>
2002-05-12 07:20:03 +0000

    Reduced format_args to adjust_msg Added prechecks of sprintf() arguments

--- Agent/Formatting.pm
+++ Agent/Formatting.pm
@@ -1,13 +1,23 @@
+###########################################################################
+# $Id: Formatting.pm,v 1.2 2002/05/12 07:20:03 wendigo Exp $
+###########################################################################
 #
-# $Id: Formatting.pm,v 1.1 2002/03/09 16:01:37 wendigo Exp $
+# Log::Agent::Formatting
 #
-#  Copyright (c) 1999, Raphael Manfredi
-#  
-#  You may redistribute only under the terms of the Artistic License,
-#  as specified in the README file that comes with the distribution.
+# RCS Revision: $Revision: 1.2 $
+# Date: $Date: 2002/05/12 07:20:03 $
 #
-# HISTORY
+# Copyright (c) 1999 Raphael Manfredi
+# Copyright (c) 2002 Mark Rogaski, mrogaski@cpan.org; all rights reserved.
+#
+# See the README file included with the
+# distribution for license information.
+#
 # $Log: Formatting.pm,v $
+# Revision 1.2  2002/05/12 07:20:03  wendigo
+# Reduced format_args to adjust_msg
+# Added prechecks of sprintf() arguments
+#
 # Revision 1.1  2002/03/09 16:01:37  wendigo
 # New maintainer
 #
@@ -17,8 +27,7 @@
 # Revision 0.2  2000/11/06 19:30:33  ram
 # Baseline for second Alpha release.
 #
-# $EndLog$
-#
+###########################################################################
 
 use strict;
 require Exporter;
@@ -34,66 +43,87 @@ use vars qw(@ISA @EXPORT_OK);
 require Log::Agent::Message;
 
 #
-# format_args
-#
-# Format arguments using sprintf() if there is more than one, taking the
-# first as the format. Otherwise, we take only its first and only argument.
-#
-# Returns a Log::Agent::Message object, which, when stringified, prints
-# the string itself.
-#
+# adjust_fmt
+# 
 # We process syslog's %m macro as being the current error message ($!) in
 # the first argument only. Doing it at this level means it will be supported
 # independently from the driver they'll choose. It's also done BEFORE any
 # log-related system call, thus ensuring that $! retains its original value.
 #
-if ($] >= 5.005) { eval q{				# if VERSION >= 5.005
+if ($] >= 5.005) {
+    eval q{     # if VERSION >= 5.005
+        # 5.005 and later version grok /(?<!)/
+        sub adjust_fmt {
+            my $fmt = shift;
+            $fmt =~ s/((?<!%)(?:%%)*)%m/$!/g;
+            return $fmt;
+        }
+    }
+} else {
+    eval q{     # else /* VERSION < 5.005 */
+        # pre-5.005 does not grok /(?<!)/
+        sub adjust_fmt {
+            my $fmt = shift;
+            $fmt =~ s/%%/\01/g;
+            $fmt =~ s/%m/$!/g;
+            $fmt =~ s/\01/%%/g;
+            return $fmt;
+        }
+    }
+}       # endif /* VERSION >= 5.005 */
 
-# 5.005 and later version grok /(?<!)/
-sub format_args {
-	my $ary = shift;
-	my $fmt = shift @$ary;
-	$fmt =~ s/((?<!%)(?:%%)*)%m/$!/g;
-	my $str = Log::Agent::Message->make(@$ary ? sprintf($fmt, @$ary) : $fmt);
-	return $str;
+#
+# whine
+#
+# This is a local hack of carp 
+#
+sub whine {
+    my $msg = shift;
+    unless (chomp $msg) {
+        my($package, $filename, $line) = caller 2;
+        $msg .= " at $filename line $line.";
+    }
+    warn "$msg\n";
 }
-
-}} else { eval q{						# else /* VERSION < 5.005 */
-
-# pre-5.005 does not grok /(?<!)/
-sub format_args {
-	my $ary = shift;
-	my $fmt = shift @$ary;
-	$fmt =~ s/%%/\01/g;
-	$fmt =~ s/%m/$!/g;
-	$fmt =~ s/\01/%%/g;
-	my $str = Log::Agent::Message->make(@$ary ? sprintf($fmt, @$ary) : $fmt);
-	return $str;
-}
-
-}}										# endif /* VERSION >= 5.005 */
 
 #
 # tag_format_args
 #
-# Same as format_args, but with extra arguments, giving a list of tags
-# to be inserted within the formatted message.
+# Arguments:
 #
-#   $caller			caller information, done firstly
-#   $priority		priority information, done secondly
-#   $tags			list of user-defined tags, done lastly
+#   $caller     caller information, done firstly
+#   $priority   priority information, done secondly
+#   $tags       list of user-defined tags, done lastly
+#   $ary        arguments for sprintf()
+#
+# Returns a Log::Agent::Message object, which, when stringified, prints
+# the string itself.
 #
 sub tag_format_args {
-	my ($caller, $priority, $tags) = splice(@_, 0, 3);
-	my $str = &format_args;
-	$caller->insert($str) if defined $caller;
-	$priority->insert($str) if defined $priority;
-	if (defined $tags) {
-		foreach my $tag (@$tags) {
-			$tag->insert($str);
-		}
-	}
-	return $str;
+    my ($caller, $priority, $tags, $ary) = @_;
+    my $msg = adjust_fmt(shift @$ary);
+
+    # This bit of tomfoolery is intended to make debugging of
+    # programs a bit easier by prechecking input to sprintf() 
+    # for errors.  I usually prefer lazy error checking, but 
+    # this seems to be an appropriate exception.
+    if (my $argcnt = grep !/%%/, $msg =~ /%./g) {
+        BEGIN { $^W = 0 }
+        if (grep {! defined} @$ary[0..($argcnt - 1)]) {
+            whine("Use of uninitialized value in sprintf");
+        }
+        $msg = sprintf $msg, @$ary;
+    }
+
+    my $str = Log::Agent::Message->make($msg);
+    $caller->insert($str) if defined $caller;
+    $priority->insert($str) if defined $priority;
+    if (defined $tags) {
+        foreach my $tag (@$tags) {
+            $tag->insert($str);
+        }
+    }
+    return $str;
 }
 
 1;
